e7e4b9dd19b81a603822fb05e6a7fed26b6ff8cf
santio89 <santiolais1989@gmail.com>
2023-01-13 14:38:27 -0300

    new-mai-landing first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+   {/*      <nav className='footer__nav'>
+            <ul>
+                <li></li>
+            </ul>
+        </nav> */}
+    </footer>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='header'>
+   {/*      <div className="header__logo">
+            <a href='./'><img alt='logo' src='#'></img></a>
+        </div>
+        <nav className='header__nav'>
+            <ul>
+                <li></li>
+            </ul>
+        </nav> */}
+    </header>
+  )
+}
